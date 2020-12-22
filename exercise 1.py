ed2d75e086f80b9b7183e2291212689f10bfcdd2
Minxuan-Hu <74728864+Minxuan-Hu@users.noreply.github.com>
2020-12-22 22:25:52 +0800

    Update exercise 1.py

--- exercise 1.py
+++ exercise 1.py
@@ -1,32 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# In[2]:
-
 
 PMF1=np.asarray([4.0,1.0,6.0,1.0,3.0,1.0,5.0,1.0])
 PMF1=PMF1/np.sum(PMF1)
 PMF2=np.asarray([2.0,3.0,2.0,5.0,5.0,2.0,3.0,2.0])
 PMF2=PMF2/np.sum(PMF2)
 
-
-# In[3]:
-
-
 #task1
 D1= np.random.choice(8,1000,p=PMF1)
 D2= np.random.choice(8,500,p=PMF2)
-
-
-# In[4]:
-
 
 #task2
 def compareHIST(D,p):
@@ -34,25 +16,13 @@ def compareHIST(D,p):
     plt.plot(range(0,8),p)
     plt.show()
 
-
-# In[5]:
-
-
 compareHIST(D1,PMF1)
 compareHIST(D2,PMF2)
-
-
-# In[6]:
-
 
 #task3
 D3=np.concatenate((D1,D2))
 PMF3=2/3*PMF1+1/3*PMF2
 compareHIST(D3,PMF3)
-
-
-# In[7]:
-
 
 #task5
 Labels=np.ones([1500])
@@ -62,16 +32,8 @@ Dataset=Dataset[:,np.random.permutation(1500)]
 d1=Dataset[0,Dataset[1,:]==1]
 d2=Dataset[0,Dataset[1,:]==2]
 
-
-# In[8]:
-
-
 compareHIST(d1,PMF1)
 compareHIST(d2,PMF2)
-
-
-# In[9]:
-
 
 #task6
 for i in range(0,8):
@@ -80,10 +42,6 @@ for i in range(0,8):
         conditional_prob=2/3*(np.count_nonzero(d1==j)/1000)/(2/3*np.count_nonzero(d1==j)/1000+1/3*np.count_nonzero(d2==j)/500)
         CP.append(conditional_prob)
     plt.bar(np.arange(8),CP)
-
-
-# In[10]:
-
 
 #task7
 #f(x)=7*x
@@ -116,4 +74,3 @@ plt.plot(x,cap,color='black',linestyle='--')
 plt.plot(x,floor,color='black',linestyle='--')
 plt.plot(x,y,color='black',linestyle='-')
 plt.show()
-
